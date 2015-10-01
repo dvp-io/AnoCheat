f9ea9b32c87c99ec8d9438ec53fbd1bb77448f74
Antoine pous <Antoine-Pous@users.noreply.github.com>
2015-10-01 13:11:24 +0200

    2.2.0
    * Prise en charge du menu dans les conversations par le mode mono (@Takka)
    * Prise en charge de l'autocomplétion, le pseudo sélectionné est souligné (@Takka)
    * Prise en charge des menus conversation et chat
    * Prise en charge du drag & drop pour les onglets de conversation

--- GeckoScript.user.js
+++ GeckoScript.user.js
@@ -7,7 +7,7 @@
 // @include       http://chat.developpez.com/
 // @include       http://46.105.99.98
 // @include       http://46.105.99.98/
-// @version       2.1.1
+// @version       2.2.0
 // @downloadURL   https://raw.githubusercontent.com/dvp-io/AnoCheat/master/GeckoScript.user.js
 // @updateURL     https://raw.githubusercontent.com/dvp-io/AnoCheat/master/GeckoScript.user.js
 // @website       http://dvp.io
@@ -32,9 +32,9 @@ AC_logAdd('success',"GeckoScript v" + GM_info.script.version + " chargé");
  * Permet d'avoir un vrai mode mono sur le chat
  */
 
-AC_addStyle('.mono,.mono :not(.ui-dialog-titlebar-close):not(.fermeture):not(#quitterChat):not(.icone):not(#reduireListe):not(#boutonMenuChat){color:#000!important;background:#fff!important;text-shadow:none!important;box-shadow:0 0!important;border-color:#000!important}.mono * .ongletActivite{border-style:dotted!important}.mono * .ongletActivite table:first-child{font-style:italic!important}.mono * .ongletMessage{border-style:dashed!important}.mono * .ongletMessage table:first-child{text-decoration:underline!important}.mono * #boutonMenuChat,.mono * #quitterChat,.mono * #reduireListe,.mono * .fermeture,.mono * .icone,.mono * .ui-dialog-titlebar-close,.mono * img,.mono img{-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);filter:gray;-webkit-filter:grayscale(100%)}.mono * .ColorPickerDivSample{display:none!important}');
-AC_buttonAdd('Mono', function() {
+AC_addStyle('.mono,.mono :not(.ui-dialog-titlebar-close):not(.fermeture):not(#quitterChat):not(.icone):not(#reduireListe):not(#boutonMenuChat):not(.horodatage):not(.cocheInvisible){color:#000!important;background:#fff!important;text-shadow:none!important;box-shadow:0 0!important;border-color:#000!important}.mono * .ongletActivite{border-style:dotted!important}.mono * .ongletActivite table:first-child{font-style:italic!important}.mono * .ongletMessage{border-style:dashed!important}.mono * .ongletMessage table:first-child{text-decoration:underline!important}.mono * #boutonMenuChat,.mono * #quitterChat,.mono * #reduireListe,.mono * .fermeture,.mono * .horodatage,.mono * .icone,.mono * .ui-dialog-titlebar-close,.mono * img,.mono img{-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);filter:gray;-webkit-filter:grayscale(100%)}.mono * .ColorPickerDivSample{display:none!important}.mono * .elementcomplsel{text-decoration:underline}.mono #menuChat a:hover,.mono #menuConversation a:hover{text-decoration:underline;border-radius:initial}.mono #menuChat,.mono #menuConversation{border:1px solid #000}.mono #menuChat * span.cocheInvisible{color:#fff}');
 
+AC_buttonAdd('Mono', function() {
   // Si le mode mono est actif  
   if(document.body.classList.contains('mono')) {
     this.style.textDecoration = 'none';     // On retire la rature
@@ -88,3 +88,73 @@ document.querySelector('#conversations').addEventListener('dblclick', function(e
     return;
   }
 });
+
+/* Drag & drop onglets
+ * Permet de réorganiser les onglets de conversation dans l'ordre qu'on souhaite
+ */
+
+// Ajout du CSS pour prévenir de la selection du texte
+AC_addStyle('[draggable]{-moz-user-select:none;-khtml-user-select: none;-webkit-user-select:none;user-select:none;-khtml-user-drag:element;-webkit-user-drag:element;}');
+
+var dndTabs = function() {
+    var cols_ = document.querySelectorAll('#barreOnglets .onglet'),
+        dragSrcEl_ = null;
+    this.handleDragStart = function (e) {
+      console.log(e);
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+      dragSrcEl_ = this;
+      this.classList.add('moving');
+    };
+    this.handleDragOver = function (e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.dataTransfer.dropEffect = 'move';
+      return false;
+    };
+    this.handleDragEnter = function (e) {
+      this.classList.add('over');
+    };
+    this.handleDragLeave = function (e) {
+      this.classList.remove('over');
+    };
+    this.handleDrop = function (e) {
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+      if (dragSrcEl_ != this) {
+         dragSrcElClass = dragSrcEl_.className;
+         dragTargetClass = this.className;
+         dragSrcEl_.innerHTML = this.innerHTML;
+         this.innerHTML = e.dataTransfer.getData('text/html');
+         this.className = dragSrcElClass;
+         dragSrcEl_.className = dragTargetClass;
+      }
+      return false;
+    };
+    this.handleDragEnd = function (e) {
+      [].forEach.call(cols_, function (col) {
+        col.classList.remove('over');
+        col.classList.remove('moving');
+      });
+    };
+    [].forEach.call(cols_, function (col) {
+      console.log(col);
+      col.setAttribute('draggable', 'true');
+      col.addEventListener('dragstart', this.handleDragStart, false);
+      col.addEventListener('dragenter', this.handleDragEnter, false);
+      col.addEventListener('dragover', this.handleDragOver, false);
+      col.addEventListener('dragleave', this.handleDragLeave, false);
+      col.addEventListener('drop', this.handleDrop, false);
+      col.addEventListener('dragend', this.handleDragEnd, false);
+    });
+}
+
+// On écoute la modification du DOM
+document.addEventListener("DOMSubtreeModified", function(ev) {
+  // Si on est sur la création d'un onglet
+  if(ev.srcElement.id == "barreOnglets") {
+    dndTabs();
+  }
+}, false);
